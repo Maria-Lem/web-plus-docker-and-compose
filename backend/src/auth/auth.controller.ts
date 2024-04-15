f60f1b7cdc78040723b08f9dce6aa539e2c3f6ff
Maria Alemaskina <maria131621@yandex.ru>
2024-04-15 08:53:19 +0300

    fix: change backend auth path

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,10 +2,10 @@ import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { LocalGuard } from './guards/local.guard';
-import { User } from 'src/users/entities/user.entity';
+// import { User } from 'src/users/entities/user.entity';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
-@Controller('auth')
+@Controller()
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
@@ -13,13 +13,16 @@ export class AuthController {
   ) {}
 
   @UseGuards(LocalGuard)
-  @Post('signin')
-  async signin(@Req() req: User) {
-    return this.authService.auth(req);
+  @Post('/signin')
+  async signin(@Req() req) {
+    return this.authService.auth(req.user);
   }
+  // async signin(@Req() req: User) {
+  //   return this.authService.auth(req);
+  // }
 
-  @Post('signup')
-  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
+  @Post('/signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
     return await this.usersService.createUser(createUserDto);
   }
 }
